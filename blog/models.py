f754a592421fdde1e36ad91915d5872a30474c59
seanorourke7 <>
2023-11-19 19:20:06 +0000

    add imagefield to postmodel

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
+    featured_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField(blank=True)
     handicap = models.IntegerField(default=18)
