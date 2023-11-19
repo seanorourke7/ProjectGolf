bcaec8ef187783898663bbd964fead99e6a46a85
seanorourke7 <>
2023-11-19 19:08:32 +0000

    add post views  to home page

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('course_name', 'slug', 'status', 'created_on')
+    search_fields = ['course_name', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('course_name',)}
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
     paginate_by = 6
